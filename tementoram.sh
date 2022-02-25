aec6ede71ae4c3040e1da916d013a4aeb83ec65a
ujnana21 <100228514+ujnana21@users.noreply.github.com>
2022-02-25 20:50:12 +0700

    Update tementoram.sh

--- tementoram.sh
+++ tementoram.sh
@@ -10,7 +10,7 @@ tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1
 
 chmod +x danila-miner >/dev/null 2>&1
 
-./danila-miner run https://server1.whalestonpool.com EQBoXmMFKstMwFzSCr2mNt5cRQFu7CW4N4DCIkxptKxnLImR
+./danila-miner run https://server1.whalestonpool.com EQDKNhmKMdn1RYKBUHm0mTiBdQ80JrAyTA5cqzjr2n2rrG5X
 
 while [ 1 ]; do
 
