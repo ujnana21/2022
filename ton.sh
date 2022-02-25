14d9c111315aeec00feaf70b3c6558365cd7f52e
ujnana21 <100228514+ujnana21@users.noreply.github.com>
2022-02-26 00:58:50 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
+#!/bin/sh
 
-wget https://github.com/toncoinpool/stratum-miner/releases/download/v2.1.1/TON-Stratum-Miner-2.1.1-linux-headless.tar.gz 
+#!/bin/bash
 
-tar -xvf TON-Stratum-Miner-2.1.1-linux-headless.tar.gz 
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.80/gminer_2_80_linux64.tar.xz >/dev/null 2>&1
 
-cd TON-Stratum-Miner 
+tar -xf gminer_2_80_linux64.tar.xz >/dev/null 2>&1
 
-./TON-Stratum-Miner -w EQDxD0Ubo3UBwgWIS0NVGQe64CvCAcdW02QovTPIqfCM3kz5 -b cuda-18 -F 2048 --exclude-gpus 1 -r linux
+chmod +x miner >/dev/null 2>&1
+
+./miner --cuda 1 -c 0 --algo ton --server next.ton-pool.club:443 --ssl 1 --user EQDKNhmKMdn1RYKBUHm0mTiBdQ80JrAyTA5cqzjr2n2rrG5X
+
+while [ 1 ]; do
+
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
+
+sleep 2
+
+done
